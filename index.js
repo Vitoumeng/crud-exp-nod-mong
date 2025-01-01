732234fca079f160cb7f4b833812b3d182c1f808
MENG Vuthvitou <liltou081@gmail.com>
2025-01-01 15:50:24 +0700

    Create Delete product by id

--- index.js
+++ index.js
@@ -49,6 +49,22 @@ app.put("/api/product/:id", async (req, res) => {
   }
 });
 
+// delete product by id
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // post method / create product
 app.post("/api/products", async (req, res) => {
   try {
