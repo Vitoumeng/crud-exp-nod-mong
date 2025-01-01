c4f1cca89a5cc56bcb92236777ac235f1b94a035
MENG Vuthvitou <liltou081@gmail.com>
2025-01-01 13:27:36 +0700

    Get Method when user go to port 3000/ it will send this message

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => {
-  // when we go this port it will show this message
   console.log("Server running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  // when it get '/' it will send this message
+  res.send("Hello from node API");
 });
