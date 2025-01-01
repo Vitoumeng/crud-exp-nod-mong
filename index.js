3bf2320313273b5b3bf4713ebe133ebb27b77cd7
MENG Vuthvitou <liltou081@gmail.com>
2025-01-01 14:21:55 +0700

    Best way to connecting to database function

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  // when we go this port it will show this message
-  console.log("Server running on port 3000");
-});
-
 app.get("/", (req, res) => {
   // when it get '/' it will send this message
   res.send("Hello from node New API");
@@ -18,6 +13,10 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
+    app.listen(3000, () => {
+      // when we go this port it will show this message
+      console.log("Server running on port 3000");
+    });
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
