638a39b93a4ac4a96a841633af3ad6160ac17acd
MENG Vuthvitou <liltou081@gmail.com>
2025-01-01 13:22:48 +0700

    Init Express and Create Listen Port when we go this port show message

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hi There!!");
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  // when we go this port it will show this message
+  console.log("Server running on port 3000");
+});
