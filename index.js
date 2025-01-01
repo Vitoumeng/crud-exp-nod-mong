bb8345e1775b1f76b789724b6f0d18bf44f26188
MENG Vuthvitou <liltou081@gmail.com>
2025-01-01 15:23:13 +0700

    Create function to get product by id from our database

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+//get product by id
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // post method
 app.post("/api/products", async (req, res) => {
   try {
